5650ef084b6044cf53ba1d5bdf26104bd3114af1
Bailey455 <90922108+Bailey455@users.noreply.github.com>
2021-09-27 14:31:59 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
+#Writes completion scores and years in a file called completion_scores.csv tab delimited
 from urllib import request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url_to_scrape = "https://www.espn.com/college-football/player/_/id/4034607/liam-welch"
+url_to_scrape = "https://www.espn.com/college-football/player/stats/_/id/4034607/liam-welch"
 
 request_page = urlopen(url_to_scrape)
 page_html = request_page.read()
@@ -10,19 +11,27 @@ request_page.close()
 
 html_soup = BeautifulSoup(page_html, 'html.parser')
 
-table = html_soup.find_all('div', class_="StatBlockInner__Value tc fw-medium n3 clr-gray-02")
+body = html_soup.find('table', class_="Table Table--align-right")
+header = html_soup.find('table', class_="Table Table--align-right Table--fixed Table--fixed-left")
 
-filename = 'products.csv'
+filename = 'completion_rates.csv'
 f = open(filename, 'w')
 
 headers = 'Completion Scores \n'
 f.write(headers)
+ 
+for heads in header.find_all('tbody'):
+    lines = heads.find_all('tr')
+    for line in lines:
+        year = line.find_all('td', class_="Table__TD")[0].text
+        f.write(year + "\t")
 
+f.write("\n")
 
-for comps in table:
-    comp_scores = table.find('div', class_="Table__TH")
-    f.write(comp_scores)
+for comps in body.find_all('tbody'):
+    rows = comps.find_all('tr')
+    for row in rows:
+        comp_scores = row.find_all('td', class_="Table__TD")[2].text
+        f.write(comp_scores + "\t")
 
 f.close()
-    
-
